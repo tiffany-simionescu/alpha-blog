b96c27dde94e4b313f413c0e6a6f391b5dfb4a61
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-07-16 18:32:45 -0400

    Updated styling of articles and carousel.

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 2021_07_11_024911) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
 end
