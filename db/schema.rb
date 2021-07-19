664c11768099927ef4431cbfbc1beab0b1ef9927
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-07-19 15:25:32 -0400

    Updated flash message styling, added users to articles, created a one-to-many relationship.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_180023) do
+ActiveRecord::Schema.define(version: 2021_07_19_185144) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
