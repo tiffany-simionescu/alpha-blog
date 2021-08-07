3e984abd7e0bd7076a5b4d3c7b8ff5f191fcf366
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-08-07 13:42:22 -0400

    Added controller and integration tests for adding new categories with admin account.

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,46 +4,58 @@ require "test_helper"
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @category = Category.create(name: "Sports")
+    @admin_user = User.create(username: "tiffany2", email: "tiffany2@email.com", 
+                              password: "password", admin: true)
   end
 
   test "should get index" do
-    get categories_url
+    get categories_path
     assert_response :success
   end
 
   test "should get new" do
-    get new_category_url
+    sign_in_as(@admin_user)
+    get new_category_path
     assert_response :success
   end
 
   test "should create category" do
+    sign_in_as(@admin_user)
     assert_difference('Category.count', 1) do
-      post categories_url, params: { category: { name: "Travel" } }
+      post categories_path, params: { category: { name: "Travel" } }
     end
 
-    assert_redirected_to category_url(Category.last)
+    assert_redirected_to category_path(Category.last)
+  end
+
+  test "should not create category if not admin" do
+    assert_no_difference('Category.count') do
+      post categories_path, params: { category: { name: "Travel" } }
+    end
+
+    assert_redirected_to categories_path
   end
 
   test "should show category" do
-    get category_url(@category)
+    get category_path(@category)
     assert_response :success
   end
 
   # test "should get edit" do
-  #   get edit_category_url(@category)
+  #   get edit_category_path(@category)
   #   assert_response :success
   # end
 
   # test "should update category" do
-  #   patch category_url(@category), params: { category: {  } }
-  #   assert_redirected_to category_url(@category)
+  #   patch category_path(@category), params: { category: {  } }
+  #   assert_redirected_to category_path(@category)
   # end
 
   # test "should destroy category" do
   #   assert_difference('Category.count', -1) do
-  #     delete category_url(@category)
+  #     delete category_path(@category)
   #   end
 
-  #   assert_redirected_to categories_url
+  #   assert_redirected_to categories_path
   # end
 end
